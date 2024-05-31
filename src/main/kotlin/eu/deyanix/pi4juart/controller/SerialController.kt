a67403658dba7ad5f321e1bf563daf2da745d31e
deyanix <deyanix@deyanix.eu>
2024-05-31 03:11:55 +0200

    Blocking read

--- src/main/kotlin/eu/deyanix/pi4juart/controller/SerialController.kt
+++ src/main/kotlin/eu/deyanix/pi4juart/controller/SerialController.kt
@@ -1,5 +1,6 @@
 package eu.deyanix.pi4juart.controller
 
+import eu.deyanix.pi4juart.service.SerialReader
 import eu.deyanix.pi4juart.service.SerialService
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
@@ -9,7 +10,20 @@ import java.io.IOException
 @RestController
 class SerialController(
 	private val serialService: SerialService,
+	private val serialReader: SerialReader,
 ) {
+	@GetMapping("/serial/read")
+	@Throws(IOException::class)
+	fun read(): String {
+		return serialReader.readLine()
+	}
+
+	@GetMapping("/serial/lines")
+	@Throws(IOException::class)
+	fun nextLines(): Array<String> {
+		return serialReader.nextLines()
+	}
+
 	@PostMapping("/serial")
 	@Throws(IOException::class)
 	fun write(@RequestBody text: String) {
---
+++ src/main/kotlin/eu/deyanix/pi4juart/service/SerialReader.kt
@@ -0,0 +1,84 @@
+package eu.deyanix.pi4juart.service
+
+import com.pi4j.io.serial.Serial
+import org.springframework.stereotype.Service
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledFuture
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.Condition
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
+
+@Service
+class SerialReader(
+	private val serial: Serial
+) {
+	private val lock: Lock = ReentrantLock()
+	private val bufferedConditional: Condition = lock.newCondition()
+	private val future: ScheduledFuture<*> = Executors.newSingleThreadScheduledExecutor()
+		.scheduleWithFixedDelay(SerialReaderRunnable(), 0, 10, TimeUnit.MILLISECONDS)
+	private val buffer: Deque<String> = LinkedList()
+	private val currentBuffer: StringBuffer = StringBuffer()
+
+	fun readLine(): String {
+		lock.lock()
+		try {
+			while (buffer.isEmpty()) {
+				bufferedConditional.await()
+			}
+			return nextLine()!!
+		} finally {
+			lock.unlock()
+		}
+	}
+
+	fun nextLine(): String? {
+		return buffer.poll()
+	}
+
+	fun nextLines(): Array<String> {
+		val lines = buffer.toTypedArray()
+		clear()
+		return lines
+	}
+
+	fun clear() {
+		buffer.clear()
+	}
+
+	inner class SerialReaderRunnable : Runnable {
+		override fun run() {
+			if (!serial.isOpen) {
+				future.cancel(false)
+			}
+
+			val length: Int = serial.available()
+			if (length == 0) {
+				return
+			}
+
+			try {
+				lock.lock()
+				val bytes = ByteArray(length)
+				serial.read(bytes, length)
+				for (byte in bytes) {
+					val char = byte.toInt().toChar()
+					if (char == '\n' || char == '\r') {
+						if (currentBuffer.isNotEmpty()) {
+							buffer.add(currentBuffer.toString())
+							currentBuffer.setLength(0)
+							bufferedConditional.signal()
+						}
+					} else {
+						currentBuffer.append(char)
+					}
+				}
+			} finally {
+				lock.unlock()
+			}
+		}
+	}
+}
