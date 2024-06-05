a9c618f1b9f0218a79a6ee6557bde49e11f137c4
deyanix <deyanix@deyanix.eu>
2024-06-05 23:59:58 +0200

    Added MongoDB

--- src/main/kotlin/eu/deyanix/smartirrigation/configurator/MqttConfigurator.kt
+++ src/main/kotlin/eu/deyanix/smartirrigation/configurator/MqttConfigurator.kt
@@ -1,13 +1,13 @@
 package eu.deyanix.smartirrigation.configurator
 
 import eu.deyanix.smartirrigation.configuration.TtnConfiguration
-import org.apache.commons.lang3.stream.IntStreams
+import org.bson.Document
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.boot.configurationprocessor.json.JSONObject
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.integration.annotation.ServiceActivator
 import org.springframework.integration.channel.DirectChannel
 import org.springframework.integration.core.MessageProducer
@@ -18,11 +18,14 @@ import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter
 import org.springframework.messaging.MessageChannel
 import org.springframework.messaging.MessageHandler
 
+
 @Configuration
 class MqttConfigurator(
-	private val ttnConfiguration: TtnConfiguration
+	private val ttnConfiguration: TtnConfiguration,
+	private val mongo: MongoTemplate
 ) {
 	private final val logger: Logger = LoggerFactory.getLogger(MqttConfigurator::class.java)
+
 
 	@Bean
 	fun clientFactory(): MqttPahoClientFactory {
@@ -61,27 +64,7 @@ class MqttConfigurator(
 	@ServiceActivator(inputChannel = "mqttInputChannel")
 	fun handler(): MessageHandler {
 		return MessageHandler {
-			val json = JSONObject(it.payload.toString())
-
-			val deviceId = json
-				.getJSONArray("identifiers")
-				.getJSONObject(0)
-				.getJSONObject("device_ids")
-				.getString("deviceId")
-
-			val messages = json.getJSONObject("data")
-				.getJSONObject("uplink_message")
-				.getJSONObject("decoded_payload")
-				.getJSONArray("messages")
-
-			IntStreams.range(messages.length())
-				.mapToObj { index -> messages.getJSONObject(index) }
-				.forEach {
-					msg -> logger.info("Sensor ("+ deviceId + "): " + msg.getInt("measurementId") + " = " + msg.getDouble("measurementValue"))
-				}
-
-			logger.info("Message (" + it.javaClass + "): " + it.payload.toString())
-			logger.info("Message headers: " + it.headers)
+			mongo.insert(Document.parse(it.payload.toString()), "ttn_up")
 		}
 	}
 }
